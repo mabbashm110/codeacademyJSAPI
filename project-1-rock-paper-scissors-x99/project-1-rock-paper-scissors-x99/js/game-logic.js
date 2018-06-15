77c3482cd924a9f87229143e8162de5abab7d87d
Mehdi Mohamed <MMohamed@rightpoint.com>
2018-06-14 21:36:50 -0500

    Updated some logic, still needs fix

--- project-1-rock-paper-scissors-x99/project-1-rock-paper-scissors-x99/js/game-logic.js
+++ project-1-rock-paper-scissors-x99/project-1-rock-paper-scissors-x99/js/game-logic.js
@@ -25,13 +25,16 @@ const setPlayerMoves = (playerName, playerMove1, playerMove1Val, playerMove2, pl
     && (gameOptions.indexOf(playerMove1) > -1) 
     && (gameOptions.indexOf(playerMove2) > -1) 
     && (gameOptions.indexOf(playerMove3) > -1)){
-        //console.log("Not null")
+
+        console.log("Vales correct and not null");
+        console.log(gameOptions.indexOf(playerMove2));
         //Check values less than 1, greater than 99, totals greater than 99.
-        if ((playerMove1Val && playerMove2Val && playerMove3Val) < 1 && 
-        (playerMove1Val + playerMove2Val + playerMove3Val) <= 99 &&
-        (playerMove1Val && playerMove2Val && playerMove3Val) <= 100)
+        
+        if ((playerMove1Val && playerMove2Val && playerMove3Val) < 1 || 
+            (playerMove1Val && playerMove2Val && playerMove3Val) <= 100 ||
+            (playerMove1Val + playerMove2Val + playerMove3Val) > 99)
         {
-            //console.log(playerMove1Val + playerMove2Val + playerMove3Val);
+            console.log(playerMove1Val + playerMove2Val + playerMove3Val);
             //Check player
             if (playerName === 'Player One'){
                 //Best practice create a player object, initiate players and assign
@@ -41,7 +44,7 @@ const setPlayerMoves = (playerName, playerMove1, playerMove1Val, playerMove2, pl
                 playerOneMoveTwoValue = playerMove2Val;
                 playerOneMoveThreeType = playerMove3;
                 playerOneMoveThreeValue = playerMove3Val;
-                //console.log(`Player One ${playerOneMoveOneType}`);
+                console.log(`Player One ${playerOneMoveOneType}`);
                 }
             else {
                 playerTwoMoveOneType = playerMove1;
@@ -50,21 +53,25 @@ const setPlayerMoves = (playerName, playerMove1, playerMove1Val, playerMove2, pl
                 playerTwoMoveTwoValue = playerMove2Val;
                 playerTwoMoveThreeType = playerMove3;
                 playerTwoMoveThreeValue = playerMove3Val;
-                //console.log(`Player Two ${playerTwoMoveOneType}`);
+                console.log(`Player Two ${playerTwoMoveOneType}`);
             }
         }
         else{
-            //console.log("null")
+            console.log("null")
         }
     }
 }
 
+setPlayerMoves('Player Two', 'rock', 11, 'paper', 23, 'scissors', 55);
+
+/*
 const getRoundWinner = (moveNumber) =>{
     switch (moveNumber){
         case 1:
 
     }
 }
+
 
 const getWinnerMove = (player1Move, player2Move) =>{
     if (player1Move === "rock"){
@@ -100,4 +107,4 @@ const getWinnerMove = (player1Move, player2Move) =>{
         }
     }
 }
-    
+*/
